a75c5e05be00641a93aea0d9dd4740c862f8897d
KL <git@kennethcalamay.com>
2015-09-30 00:53:22 +0800

    Add update interface to the todo app

--- simple_todo.ex
+++ simple_todo.ex
@@ -1,11 +1,41 @@
 defmodule TodoList do
-  def new, do: HashDict.new
+  defstruct auto_id: 1, entries: HashDict.new
 
-  def add_entry(todo_list, entry) do
-    MultiDict.add(todo_list, entry.date, entry)
+  def new, do: %TodoList{}
+
+  def add_entry(
+    %TodoList{entries: entries, auto_id: auto_id} = todo_list,
+    entry
+  ) do
+    entry = Map.put(entry, :id, auto_id)
+    new_entries = HashDict.put(entries, auto_id, entry)
+
+    %TodoList{ todo_list | entries: new_entries, auto_id: auto_id + 1 }
   end
 
-  def entries(todo_list, date) do
-    MultiDict.get(todo_list, date)
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn(_) -> new_entry end)
+  end
+
+  def update_entry(
+    %TodoList{entries: entries} = todo_list,
+    entry_id,
+    updater_fun
+  ) do
+    case entries[entry_id] do
+      nil -> todo_list
+
+      old_entry ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entries = HashDict.put(entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def entries(%TodoList{entries: entries}, date) do
+    entries
+    |> Stream.filter(fn({_, entry}) -> entry.date == date end)
+    |> Enum.map(fn({_, entry}) -> entry end)
   end
 end
